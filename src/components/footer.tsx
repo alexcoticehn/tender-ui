9550ac2d0617f2d1c836d1a0f7b2aa8e05270fad
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2026-03-22 19:47:29 -0700

    cleanup colors in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-      <footer className="bg-neutral-950 text-neutral-300 pt-12 pb-6 mt-16 border-t border-neutral-300">
+      <footer className="bg-neutral-950 text-neutral-400 pt-12 pb-6 mt-16 border-t border-neutral-300">
         <div className="px-4 md:px-6 lg:px-10 max-w-7xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8 text-center md:text-left">
           {/* Left: Business Blurb */}
@@ -13,8 +13,8 @@ export default function Footer() {
             </p>
           </div>
           {/* Right: Page Links in Two Columns */}
-          <nav className="flex flex-col gap-2 text-sm hover:text-neutral-50">
-            <Link href="/" className=" transition-colors">Home</Link>
+          <nav className="flex flex-col gap-2 text-sm">
+            <Link href="/" className="hover:text-neutral-50 transition-colors">Home</Link>
             <Link href="/services" className="hover:text-neutral-50 transition-colors">Services</Link>
             <Link href="/about" className="hover:text-neutral-50 transition-colors">About</Link>
             <Link href="/emdr" className="hover:text-neutral-50 transition-colors">EMDR</Link>
@@ -26,7 +26,7 @@ export default function Footer() {
           </nav>
           </div>
           {/* Bottom: Copyright & Land Acknowledgement */}
-          <div className="mt-10 border-t border-neutral-300 pt-6 text-neutral-50">
+          <div className="mt-10 border-t border-neutral-400 pt-6 text-neutral-400">
           <p className="text-xs">&copy; 2026 Tender Counselling. All rights reserved.</p>
           <p className="mt-2 text-xs">
             I respectfully acknowledge that I occupy the unceded, ancestral territories of the xʷməθkwəy̓əm (Musqueam), Skwxwú7mesh (Squamish), S’ólh Téméxw (Stó:lō), Səl̓ílwətaʔ/Selilwitulh (Tsleil-Waututh), Qayqayt, and kʷikʷəƛ̓əm (Kwikwetlem) peoples.
